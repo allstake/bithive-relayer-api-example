80b8dffcb05e72b08f567c14543c6bbc447815af
roblint <171213773+roblint@users.noreply.github.com>
2024-12-25 10:24:52 +0800

    fix: fee example

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import 'dotenv/config';
 import { run as stake } from './example/stake';
 import { run as unstake } from './example/unstake';
+import { run as fee } from './example/fee';
 import { run as staker } from './example/staker';
 
 const args = process.argv.slice(2);
@@ -10,6 +11,8 @@ if (example === 'stake') {
   stake().catch(console.error);
 } else if (example === 'unstake') {
   unstake().catch(console.error);
+} else if (example === 'fee') {
+  fee().catch(console.error);
 } else if (example === 'staker') {
   staker().catch(console.error);
 } else {
