7f4aadd61588eb3d4af4046550f5827d2ff8208d
roblint <171213773+roblint@users.noreply.github.com>
2024-12-25 10:16:36 +0800

    refactor: options

--- src/utils/staker.ts
+++ src/utils/staker.ts
@@ -9,6 +9,12 @@ import {
 } from './relayer';
 import { BitcoinSigner } from './signer';
 
+type TxOptions = {
+  fee?: number;
+  feeRate?: number;
+  wait?: boolean;
+};
+
 export class BitHiveStaker {
   signer: BitcoinSigner;
 
@@ -16,39 +22,45 @@ export class BitHiveStaker {
     this.signer = signer;
   }
 
-  async stake(
-    amount: number,
-    options?: { fee?: number; feeRate?: number; wait?: boolean },
-  ) {
+  async stake(amount: number, { fee, feeRate, wait = true }: TxOptions = {}) {
     const publicKey = this.signer.getPublicKey();
     const address = this.signer.getAddress();
+
     console.log(`Staking ${satToBtc(amount)} BTC...`);
     const txHash = await stake(this.signer, publicKey, address, amount, {
-      fee: options?.fee,
-      feeRate: options?.feeRate,
+      fee,
+      feeRate,
     });
     console.log(`Staking transaction broadcasted: ${txUrl(txHash)}`);
-    if (options?.wait !== false) {
+
+    if (wait) {
       await waitUntilStaked(publicKey, txHash);
     }
+
     return txHash;
   }
 
-  async unstake(depositTxHash: string, options?: { wait?: boolean }) {
+  async unstake(
+    depositTxHash: string,
+    { wait = true }: { wait?: boolean } = {},
+  ) {
     const publicKey = this.signer.getPublicKey();
+
     console.log('Unstaking BTC...');
     await unstake(this.signer, publicKey, depositTxHash);
-    if (options?.wait !== false) {
+
+    if (wait) {
       await waitUntilUnstaked(publicKey, depositTxHash);
     }
   }
 
   async withdraw(
     depositTxHash: string,
-    options?: { fee?: number; feeRate?: number; wait?: boolean },
+    { fee, feeRate, wait = true }: TxOptions = {},
   ) {
     const publicKey = this.signer.getPublicKey();
     const address = this.signer.getAddress();
+
     console.log('Withdrawing BTC...');
     const withdrawalTxHash = await withdraw(
       this.signer,
@@ -56,16 +68,18 @@ export class BitHiveStaker {
       address,
       depositTxHash,
       {
-        fee: options?.fee,
-        feeRate: options?.feeRate,
+        fee,
+        feeRate,
       },
     );
     console.log(
       `Withdrawal transaction broadcasted: ${txUrl(withdrawalTxHash)}`,
     );
-    if (options?.wait !== false) {
+
+    if (wait) {
       await waitUntilWithdrawn(publicKey, depositTxHash);
     }
+
     return withdrawalTxHash;
   }
 }
