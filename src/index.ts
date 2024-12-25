0e7ff1739d41b84f312de2a488489d96e2902d50
roblint <171213773+roblint@users.noreply.github.com>
2024-12-25 11:31:32 +0800

    refactor: entry point

--- src/index.ts
+++ src/index.ts
@@ -4,17 +4,21 @@ import { run as unstake } from './example/unstake';
 import { run as fee } from './example/fee';
 import { run as staker } from './example/staker';
 
-const args = process.argv.slice(2);
-const example = args[0];
+async function main() {
+  const args = process.argv.slice(2);
+  const example = args[0];
 
-if (example === 'stake') {
-  stake().catch(console.error);
-} else if (example === 'unstake') {
-  unstake().catch(console.error);
-} else if (example === 'fee') {
-  fee().catch(console.error);
-} else if (example === 'staker') {
-  staker().catch(console.error);
-} else {
-  console.error('Invalid example');
+  if (example === 'stake') {
+    await stake();
+  } else if (example === 'unstake') {
+    await unstake();
+  } else if (example === 'fee') {
+    await fee();
+  } else if (example === 'staker') {
+    await staker();
+  } else {
+    console.error('Invalid example');
+  }
 }
+
+main().catch(console.error);
